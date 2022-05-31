c1dded8467de02fa90f6798c34aa3838ca878c50
Rodrigo Ferreira <rodrigoferreira@MacBook-Pro-de-Rodrigo.local>
2022-05-31 01:52:04 -0300

    feat: mac support

--- julia-cat.py
+++ julia-cat.py
@@ -1,20 +1,23 @@
 import cv2
 import numpy as np
-import sacad
 import os
 from moviepy.editor import *
 from requests import get
 from yt_dlp import YoutubeDL
 
 
-#search string on youtube and download mp3
+# search string on youtube and download mp3
 def download_mp3(search_string):
 
-    dl = YoutubeDL({'outtmpl': 'temp.mp3'})
+    dl = YoutubeDL({'format': 'bestaudio/best', 'outtmpl': 'temp.mp3', 'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '192',
+    }]})
     dl.download(['ytsearch:'+search_string])
     mp3_file = 'temp.mp3'
     return mp3_file
-    
+
 
 album = input("Enter album name: ")
 artist = input("Enter artist name: ")
@@ -28,50 +31,52 @@ search_term = artist + " " + song
 
 found = download_mp3(search_term)
 
-print(found)
-
 image = cv2.imread("temp.jpg")
- 
-fourcc = cv2.VideoWriter_fourcc('M','J','P','G')
 
-out = cv2.VideoWriter('temp.avi',fourcc, 30, (640,460))
+fourcc = cv2.VideoWriter_fourcc('M', 'J', 'P', 'G')
+
+out = cv2.VideoWriter('temp.avi', fourcc, 30, (640, 460))
+
+print("[INFO] Removing green screen...")
 
 while True:
-    
+
     ret, frame = video.read()
     if(not ret):
         break
     frame = cv2.resize(frame, (640, 480))
     image = cv2.resize(image, (640, 480))
- 
-    hsv = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
-    lower_green = np.array([0, 180, 0])
-    green = np.array([255,255,255])
 
-    mask = cv2.inRange(hsv,lower_green,green)
-    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (4,4))
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    lower_green = np.array([0, 180, 0])
+    green = np.array([255, 255, 255])
+
+    mask = cv2.inRange(hsv, lower_green, green)
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (4, 4))
     dilate = cv2.dilate(mask, kernel, iterations=3)
-    res = cv2.bitwise_and(frame,frame, mask = dilate)
-    
+    res = cv2.bitwise_and(frame, frame, mask=dilate)
+
     f = frame - res
     f = np.where(f == 0, image, f)
 
     f = f[10:470, 0:640]
 
     out.write(f)
- 
+
 out.release()
 
-videoclip = VideoFileClip("temp.avi").subclip(0,28)
+videoclip = VideoFileClip("temp.avi").subclip(0, 28)
 
-audioclip = AudioFileClip("temp.mp3").subclip(28,56)
+audioclip = AudioFileClip("temp.mp3").subclip(28, 56)
 
 
 new_audioclip = CompositeAudioClip([audioclip])
 videoclip.audio = new_audioclip
 
+print("[INFO] Removing temp files...")
+
 os.remove("temp.jpg")
 os.remove("temp.avi")
 os.remove("temp.mp3")
 
-videoclip.write_videofile("output.mp4")
+videoclip.write_videofile("output.mp4", audio_codec='aac')
